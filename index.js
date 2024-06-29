05f560baf114e708c52f7d90ae2797277ac974f2
Emre Kadir Tiren <77925614+EmreKadirTiren@users.noreply.github.com>
2024-06-29 12:19:59 +0200

    Pyramid Function + Prefixes
    I made a command that if called makes pyramids. And i added the ! prefix to all of my commands.

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
 const { Client, GatewayIntentBits } = require('discord.js');
+
+dotenv.config();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+client.login(process.env.DISCORD_TOKEN);
 prefix = '!';
-const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent,
-    ],
-});
 
+// When the bot is ready, log to the console
 client.once('ready', () => {
-    console.log('Bot is online!');
+    console.log(`Bot is online! And logged in as ${client.user.tag}!`);
 });
 
+// If ping is sent, reply with pong
 client.on('messageCreate', message => {
-    if (message.content === 'ping') {
+    if (message.content === `${prefix}ping`) {
         message.channel.send('pong');
     }
 });
 
+// If someone says nee reply with nee 
 client.on('messageCreate', message => {
-    if (message.content === 'nee') {
+    if (message.author.bot) return;
+    if (message.content === `${prefix}nee`) {
       message.reply('nee');
     }
-  });
-  
+});
+
+
+
+// if somone runs the command !pyramid 5, it will return a pyramid with 5 rows
+client.on('messageCreate', message => {
+    if (message.content.startsWith(`${prefix}pyramid`)) {
+        const args = message.content.split(' ');
+        const size = parseInt(args[1]);
+
+        if (!size || size <= 0) {
+            return message.channel.send('Please provide a valid positive number for the pyramid size.');
+        }
+
+        let pyramid = '';
+        for (let i = 1; i <= size; i++) {
+            pyramid += ' '.repeat(size - i) + `#`.repeat(i * 2 - 1) + '\n';
+        }
+
+       message.channel.send(`\`\`\`${pyramid}\`\`\``);
+       //message.channel.send(pyramid);
+    }
+});
 
 client.login(process.env.DISCORD_TOKEN);
